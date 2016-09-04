cd92bdceaeb52088fd62e4aca524e672b1853dc8
Anton Medvedev <anton@medv.io>
2016-09-05 00:31:48 +0700

    Allow any number of params in fx function.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import { Chain } from './chain';
 export { range } from './range';
 
 /**
- * @param iterator
+ * @param {*} params
  * @returns {Chain}
  */
-export function fx(iterator) {
-  return new Chain(iterator);
+export function fx(...params) {
+  return new Chain(params.length === 1 ? params[0] : params);
 }
